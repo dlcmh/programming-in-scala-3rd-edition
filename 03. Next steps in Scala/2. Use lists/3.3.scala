23bb45cab35c58f3cf4e7a9e728992cc88ed9593
David Chin <dlcmhd@me.com>
2017-12-04 16:55:40 +0800

    .

--- 03. Next steps in Scala/2. Use lists/3.3.scala
+++ 03. Next steps in Scala/2. Use lists/3.3.scala
@@ -1,7 +1,9 @@
 val oneTwo = List(1, 2)
 val threeFour = List(3, 4)
 
-val oneTwoThreeFour = oneTwo ::: threeFour // ::: does list concatenation
+// ::: does list concatenation - pronounced as "cons"
+// in this example, ::: prepends oneTwo to threeFour
+val oneTwoThreeFour = oneTwo ::: threeFour
 
 println(oneTwo + " and " + threeFour + " were not mutated.")
 println("Thus, " + oneTwoThreeFour + " is a new list.")
